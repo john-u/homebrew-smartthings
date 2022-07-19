e83c006095f3eea5ab3e042005e057b3df21008a
John Undersander <19363370+john-u@users.noreply.github.com>
2022-07-19 09:44:07 -0500

    test bump

--- Formula/smartthings.rb
+++ Formula/smartthings.rb
@@ -1,9 +1,13 @@
 class Smartthings < Formula
-  desc "Command Line Interface for the SmartThings APIs"
-  homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
-  url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@1.0.0-beta.11/smartthings-macos.tar.gz"
-  sha256 "c91a5d1c51c51002f98e0342d18cef770ccc86f50532b480edfb1666d6667002"
+  desc "Command-line Interface for the SmartThings APIs"
+  homepage "https://github.com/john-u/smartthings-cli"
+  url "https://github.com/john-u/smartthings-cli/releases/download/@smartthings/cli@1.0.0-beta.10/smartthings-macos.tar.gz"
+  sha256 "b68e64793b46eb47fe3423947a432aad867c909e635ffa4a7baeeaf026b15be0"
   license "Apache-2.0"
+
+  livecheck do
+    url :url
+  end
 
   def install
     bin.install "smartthings"
